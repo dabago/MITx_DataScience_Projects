446aafe7734348b445fa37634d0ba165d9169fa1
Dv <dabago@student.ie.edu>
2020-02-22 21:16:18 +0100

    final try

--- Unit1/HW1.1.py
+++ Unit1/HW1.1.py
@@ -123,3 +123,68 @@ for d, sample in enumerate(X):
 
 # Print a possible hyperplane, that is seperating the two classes.
 plt.plot([-2,6],[6,0.5])
+
+
+#%%
+
+X = np.array([
+    [-1,1],
+    [-1,-1],
+    [1,1],
+    [2,2],
+
+
+])
+
+y = np.array([1,1,-1,-1])
+
+# X = X[::-5]
+# y = y[::-5]
+
+
+
+
+
+# Xx = np.random.shuffle(X)
+
+print('This is X')
+print(X)
+print('.......')
+def perceptron_sgd(Xx, Y):
+    w = np.zeros(2)
+    eta = 1
+    epochs = 4
+    # print(w)
+    # combination=[]
+    miss=[]
+    NotOK=0
+    OK = 0 
+    for t in range(epochs):
+
+        for i, x in enumerate(X):
+            # print('This is i')
+            # print(i)
+            # print('This is x')
+            # print(x)
+            if (np.dot(X[i], w)*Y[i]) <= 0:
+                # print(np.dot(X[i+1], w)*Y[i+1])
+                print('+++')
+                # print('not ok')
+                print(X[i])
+                miss.append(X[i])
+                w = w + eta*X[i]*Y[i]
+                print('Nuevo W')
+                print(w)
+                NotOK= NotOK + 1
+            else:
+                print('OK')
+                print(X[i])
+                OK = OK+1
+                
+    print(NotOK)
+    print(OK)
+    # print(miss)
+    return w,miss
+
+w = perceptron_sgd(X,y)
+print(w)
